8ea6d5be7287afb1911c5a1510bd76392a99f717
jaskiratr <randj063@newschool.edu>
2016-05-07 12:32:35 -0400

    io

--- app.js
+++ app.js
@@ -50,10 +50,10 @@ var app = express();
 var server = require('http').Server(app);
 
 // var io = require('socket.io').listen(server);
-// var io = require('socket.io')({
-//     "transports" : ["xhr-polling"],
-//     "polling duration" : 10
-// }).listen(server);
+var io = require('socket.io')({
+    "transports" : ["xhr-polling"],
+    "polling duration" : 10
+}).listen(server);
 
 server.listen(process.env.PORT);
 // io.set('transports', ['websocket', 
